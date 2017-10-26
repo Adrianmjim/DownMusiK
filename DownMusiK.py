b6e5816a6b4a366b80fb4c5296bd5047648aada3
Adrian <adrian@debian.Movildat.com>
2017-10-26 13:21:34 +0200

    terminado

--- DownMusiK.py
+++ DownMusiK.py
@@ -1,10 +1,12 @@
 import pafy
 from pydub import AudioSegment
 import os
+os.mkdir("MusiK")
+print "ONLY TEKNO ALLOWED"
 list = pafy.get_playlist("https://www.youtube.com/playlist?list=PLgLQAHLZtaRrenlDBCkwc2Rc-SR4Q_lQw")
 for i in list["items"]:
-	print "Descargando..."+i["pafy"].title
+	print "Downloading and converting..."+i["pafy"].title
 	filename = i["pafy"].getbestaudio().download(quiet=False)
-	print "Convirtiendo..."
 	AudioSegment.from_file(filename).export("MusiK/"+i["pafy"].title+".mp3", format="mp3")
-	os.remove(filename)	
+	os.remove(filename)
+print "Ale, gozatelo!"
