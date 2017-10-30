604a756ca315961bd12d07355a0e47e3d23f7500
Adrian <adrian@debian.Movildat.com>
2017-10-30 11:06:28 +0100

    Url changed

--- DownMusiK.py
+++ DownMusiK.py
@@ -1,9 +1,20 @@
 import pafy
 from pydub import AudioSegment
 import os
+print "Que quieres descargar?"
+print "1. Lista predeterminanda"
+print "2. Introducir la url de tu lista."
+opcion = int(input("Opcion: "))
+if opcion == 1:
+	lista = "https://www.youtube.com/playlist?list=PLgLQAHLZtaRrenlDBCkwc2Rc-SR4Q_lQw"
+elif opcion == 2:
+	lista = raw_input("Introduce aqui tu lista: ")
+else:
+	print "Opcion erronea. Vuelve a ejecutar"
+	exit()
 os.mkdir("MusiK")
 print "ONLY TEKNO ALLOWED"
-list = pafy.get_playlist("https://www.youtube.com/playlist?list=PLgLQAHLZtaRrenlDBCkwc2Rc-SR4Q_lQw")
+list = pafy.get_playlist(lista)
 for i in list["items"]:
 	print "Downloading and converting..."+i["pafy"].title
 	filename = i["pafy"].getbestaudio().download(quiet=False)
