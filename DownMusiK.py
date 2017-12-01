cb5241ac3a47995efe86c82a40874cd1bb36af86
Adrianmjim <adrima05@ucm.es>
2017-12-01 11:24:09 +0100

    Python 3

--- DownMusiK.py
+++ DownMusiK.py
@@ -1,23 +1,23 @@
 import pafy
 from pydub import AudioSegment
 import os
-print "Que quieres descargar?"
-print "1. Lista predeterminanda"
-print "2. Introducir la url de tu lista."
+print ("Que quieres descargar?")
+print ("1. Lista predeterminanda")
+print ("2. Introducir la url de tu lista.")
 opcion = int(input("Opcion: "))
 if opcion == 1:
 	lista = "https://www.youtube.com/playlist?list=PLgLQAHLZtaRrenlDBCkwc2Rc-SR4Q_lQw"
 elif opcion == 2:
 	lista = raw_input("Introduce aqui tu lista: ")
 else:
-	print "Opcion erronea. Vuelve a ejecutar"
+	print ("Opcion erronea. Vuelve a ejecutar")
 	exit()
 os.mkdir("MusiK")
-print "ONLY TEKNO ALLOWED"
+print ("ONLY TEKNO ALLOWED")
 list = pafy.get_playlist(lista)
 for i in list["items"]:
-	print "Downloading and converting..."+i["pafy"].title
+	print ("Downloading and converting..."+i["pafy"].title)
 	filename = i["pafy"].getbestaudio().download(quiet=False)
-	AudioSegment.from_file(filename).export("MusiK/"+i["pafy"].title+".mp3", format="mp3")
+	AudioSegment.from_file(filename).export("MusiK/"+title+".mp3", format="mp3")
 	os.remove(filename)
-print "Ale, gozatelo!"
+print ("Ale, gozatelo!")
